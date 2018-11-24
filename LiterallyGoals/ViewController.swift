3215fcada7d76284aff21e5d3c1256602ddb2faf
Sean Saoirse <seansaoirse@icloud.com>
2018-11-24 20:14:39 +0700

    Initial Commit

---
+++ LiterallyGoals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiterallyGoals
+//
+//  Created by Sean Saoirse on 24/11/18.
+//  Copyright © 2018 Sean Saoirse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
